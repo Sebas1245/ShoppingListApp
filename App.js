5ecc92d106ed193f4d36408d9d52db1ce2fb27cb
Sebastián Saldaña <ssaldanacardenas@gmail.com>
2022-02-01 12:54:13 -0600

    Adds ListItem with delete action

--- App.js
+++ App.js
@@ -1,11 +1,32 @@
-import React from 'react';
-import {View, StyleSheet} from 'react-native';
+import React, {useState} from 'react';
+import {View, StyleSheet, FlatList} from 'react-native';
 import Header from './components/Header';
+import ListItem from './components/ListItem';
+import uuid from 'react-native-uuid';
 
 const App = () => {
+  const [items, setItems] = useState([
+    {id: uuid.v4(), text: 'Milk'},
+    {id: uuid.v4(), text: 'Eggs'},
+    {id: uuid.v4(), text: 'Bread'},
+    {id: uuid.v4(), text: 'Juice'},
+  ]);
+
+  const deleteItem = id => {
+    setItems(prevItems => {
+      return prevItems.filter(item => item.id !== id);
+    });
+  };
   return (
     <View style={styles.container}>
       <Header />
+      {/* FlatList is equivalent to table view in iOS */}
+      <FlatList
+        data={items}
+        renderItem={({item}) => (
+          <ListItem item={item} deleteItem={deleteItem} />
+        )}
+      />
     </View>
   );
 };
